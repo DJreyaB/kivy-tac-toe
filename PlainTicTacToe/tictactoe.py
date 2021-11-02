eba13cad405ae87de544c983fa7a45971dfd8591
djreyab <djreya.boyd@gmail.com>
2021-11-01 22:15:49 -0400

    outline of project created

---
+++ PlainTicTacToe/tictactoe.py
@@ -0,0 +1,27 @@
+'''
+
+3 X 3 nested arrays for board
+1 board of '' if empty, else filled with value
+
+'''
+# New Game Board Creation
+
+# Board User Sees
+
+
+# Ask User for X or or
+
+# Generate random number from 1 - 10 for a target then for computer guess 
+#    ask user to guess. closest to number goes first(abs(player - target), abs(comp - target))
+
+# Keep a set of 1 -9 for available spaces and pop off spaces as they're occupied, 
+# verify that the users choice is in the set before accepting placement
+
+# Computers placement is determined by random number generator of possible values 
+
+# After set of available places is less than 5 begin checking for winners 
+#   When winner found add a point to score ask player 
+
+#   If no winner is found and available spaces len > 0 
+#       continue 
+#   otherwise cats game
